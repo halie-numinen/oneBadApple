e402028bafcaa4a629f7ef40194012419033c7a6
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-02-25 13:46:58 -0500

    adding in some of the code I have been working on, this wont compile yet.

--- oneBadApple.c
+++ oneBadApple.c
@@ -11,43 +11,82 @@
 void sigHandler (int);
 
 int main() {
-    int i = 0 //counting for the nodes
-    int counter = 0 //while loop variable
-    int fd[2];
-    int pid;
-    int pipeCreationResult;
-    pipeCreationResult = pipe(fd);
-    if (pipeCreationResult < 0) {
-        perror("Failed pipe creation\n");
-        exit(1);
-    }
-    pid = fork();
-    if (pid < 0) {
-        perror("Fork failed\n");
-        exit(1);
-    }
-
+    // int i = 0; //counting for the nodes
+    // int counter = 0; //while loop variable
+    char nodesString[20];
+    long nodes;
+    long destinationNodeInteger;
+    char* noNode;
+    char *errorCheacking;
     while (1) { // user input for number of nodes
         printf("Enter number of nodes: ");
-        fgets(int, n, stdin);
+        fgets(nodesString, sizeof(nodesString) stdin);
+        if (strlen(nodesString) > 0 || noNode == NULL) {
+            break;
+        }
     }
+    nodesString[strcspn(nodesString, "\n")] = '\0';
+    nodes = strtol(nodesString, &errorCheacking, 10);
 
-    while (counter < n){ // creating nodes from given input
-        struct node i
-        {
-            struct node *next
-        };
-        n++
-        i++
+    int fd[nodes][2];
+    int pid;
+    int inputPipe, outputPipe;
+    char message[1000];
+    char destinationNode[20];
+    int pipeCreationResult;
+    for (int i = 0; i < nodes; i++) { // creating pipes
+        pipeCreationResult = pipe(fd);
+        if (pipeCreationResult < 0) {
+            perror("Failed pipe creation\n");
+            exit(1);
+        }
+        printf("Parent created node %d with a pid of: %d\n", i, getpid());
+
     }
-
     while (1) { // getting the message the user what to send and what node to send to
         printf("Enter message: ");
-        fgets(char, message, stdin);
-        printf("Enter desired node to receive message: "):
-        fgets(int, nodenum, stdin);
-
+        char* noDestination = fgets(destinationNode, sizeof(destinationNode), stdin);
+        printf("Enter desired node to receive message: ");
+        char* noMessage = fgets(message, 1000, stdin);
+        if (noMessage == NULL || noDestination == NULL || strlen(destinationNode) > 0) {
+            break;
+        }
     }
+    destinationNode[strcspn(destinationNode, "\n")] = '\0';
+    destinationNodeInteger = strtol(destinationNode, &errorCheacking, 10);
+
+    char readMessage[1000];
+    int childId;
+    message[strcspn(message, "\n")] = '\0';
+    for (int i = 0; i < nodes - 1; i++) { 
+        pid = fork();
+        if (pid < 0) {
+            perror("Fork failed\n");
+            exit(1);
+        }
+        if (pid == 0) {
+            childId = i;
+            inputPipe = fd[(childId - 1 + nodes) % nodes][READ]; 
+            outputPipe = fd[nodes][WRITE];
+        
+            for (int j = 0; j < nodes; j++) { // closing unuesd ends
+                if (fd[j][READ] != inputPipe) {
+                    close(fd[j][READ]); // what the child uses
+                }
+                if (fd[j][WRITE] != outputPipe) {
+                    close(fd[j][WRITE]);
+                }
+        }
+    
+    }
+    while (counter < n){ // creating nodes from given input
+        struct node[i] {
+            struct node *next;
+        };
+        n++;
+        i++;
+    }
+
     
     string[strcspn(string, "\n") = '\0'];
     signal (SIGINT, sigHandler);
